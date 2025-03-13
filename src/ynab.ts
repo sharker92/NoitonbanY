bce63a707a755188028ddeb6a700c1b031df101a
Andrés Morales Reyes <sharker92@hotmail.com>
2025-03-13 10:01:23 -0600

    👌 IMPROVE: Added more columns to db and send it's parameters.

--- src/ynab.ts
+++ src/ynab.ts
@@ -1,5 +1,19 @@
 import * as ynab from 'ynab';
 import { Client } from '@notionhq/client';
+import { writeFile } from 'fs/promises'; // TODO: guardar en un archivo todos las transacciones para analizarlas y ver todas las posibilidades sobre todo las split transactions 13/03/24
+// (async () => {
+//     try {
+//         // Convert the object to a JSON string
+//         const jsonString = JSON.stringify(myObject, null, 2); // Pretty format with indentation
+//
+//         // Write the JSON string to a file
+//         await writeFile('myData.json', jsonString);
+//
+//         console.log('File has been saved successfully!');
+//     } catch (err) {
+//         console.error('Error writing to file:', err);
+//     }
+// })();
 
 import dotenv from 'dotenv';
 
@@ -19,6 +33,7 @@ const ynabAPI = new ynab.API(process.env.YNAB_KEY);
   console.log(transactions.length);
   console.log(transactionsServerKnowledge);
   const notion = new Client({ auth: process.env.NOTION_KEY });
+  const transaction = transactions[11];
   const response = await notion.pages.create({
     parent: { database_id: process.env.NOTION_YNAB_DATABASE_ID },
     properties: {
@@ -27,7 +42,7 @@ const ynabAPI = new ynab.API(process.env.YNAB_KEY);
         title: [
           {
             type: 'text',
-            text: { content: transactions[10].memo, link: null }, //TODO: SIgue mandar el transactions[0] hehe ya logre crear la pagina en notion :) https://developers.notion.com/reference/post-page
+            text: { content: transaction.memo, link: null }, //TODO: SIgue mandar el transactions[0] hehe ya logre crear la pagina en notion :) https://developers.notion.com/reference/post-page
             annotations: {
               bold: false,
               italic: false,
@@ -40,15 +55,27 @@ const ynabAPI = new ynab.API(process.env.YNAB_KEY);
       },
       'Transaction Date': {
         date: {
-          start: transactions[10].date,
+          start: transaction.date,
         },
       },
       Amount: {
-        number: transactions[10].amount / 1000,
+        number: transaction.amount / 1000,
       },
-      Payee: { select: { name: transactions[10].payee_name } }, //TODO: Revisa si el color se pone por defecto o cambia.
-      Category: { select: { name: transactions[10].category_name } },
-      Account: { select: { name: transactions[10].account_name } },
+      Payee: { select: { name: transaction.payee_name } }, //TODO: Revisa si el color se pone por defecto o cambia.
+      Category: { select: { name: transaction.category_name } },
+      Account: { select: { name: transaction.account_name } },
+      ynab_id: {
+        rich_text: [{ text: { content: transaction.id } }],
+      },
+      account_id: {
+        rich_text: [{ text: { content: transaction.account_id } }],
+      },
+      payee_id: {
+        rich_text: [{ text: { content: transaction.payee_id } }],
+      },
+      category_id: {
+        rich_text: [{ text: { content: transaction.category_id } }],
+      },
     },
   });
   console.log(response);
@@ -59,14 +86,42 @@ const ynabAPI = new ynab.API(process.env.YNAB_KEY);
 })();
 // TODO: load everything in notion and do a process to get the last server_knowledge to just pull every day the last transactions.
 // {
-//   id: 'a1b71d00-6158-4e7d-b9e0-ad7c79e66516',
+//   id: '01028d15-32ec-473a-a096-84bbfe83bc06', NOTE: ✅
+//   date: '2023-09-02', NOTE: ✅
+//   amount: -990080, NOTE: ✅
+//   memo: '', NOTE: ✅
+//   cleared: 'reconciled',
+//   approved: true,
+//   flag_color: undefined,
+//   flag_name: undefined,
+//   account_id: '5afbbcbc-fca0-4404-bd06-d914cd21e9f5', NOTE: ✅
+//   payee_id: '5f77e6dd-09ec-40fc-8a01-6d2bcabf8b74', NOTE: ✅
+//   category_id: 'fad59901-5dc1-4108-8ff2-14b4fa39bdb5', NOTE: ✅
+//   transfer_account_id: undefined,
+//   transfer_transaction_id: undefined,
+//   matched_transaction_id: undefined,
+//   import_id: undefined,
+//   import_payee_name: undefined,
+//   import_payee_name_original: undefined,
+//   debt_transaction_type: undefined,
+//   deleted: false,
+//   account_name: 'Platino',NOTE: ✅
+//   payee_name: 'SAMS',NOTE: ✅
+//   category_name: '🫒 Alacena',NOTE: ✅
+//   subtransactions: []
+// }
+// {
+//   id: 'efa245d0-8af3-4782-bfc9-32cfeb17788b',
+//   date: '2023-09-02',
+//   amount: -392830,
+//   memo: '',
 //   cleared: 'reconciled',
 //   approved: true,
 //   flag_color: undefined,
 //   flag_name: undefined,
 //   account_id: '5afbbcbc-fca0-4404-bd06-d914cd21e9f5',
-//   payee_id: '0e3f890b-021f-43a3-82b4-5b2144314aa2',
-//   category_id: '53ce3b8f-6cc0-4db6-8c64-eac6238ef7d6',
+//   payee_id: '754cbec3-319a-48f6-95dd-91bddd527667',
+//   category_id: 'fad59901-5dc1-4108-8ff2-14b4fa39bdb5',
 //   transfer_account_id: undefined,
 //   transfer_transaction_id: undefined,
 //   matched_transaction_id: undefined,
@@ -76,8 +131,8 @@ const ynabAPI = new ynab.API(process.env.YNAB_KEY);
 //   debt_transaction_type: undefined,
 //   deleted: false,
 //   account_name: 'Platino',
-//   payee_name: 'Starting Balance',
-//   category_name: 'Inflow: Ready to Assign',
+//   payee_name: 'Costco',
+//   category_name: '🫒 Alacena',
 //   subtransactions: []
 // }
 
