9f43f6d7b2fd3ecb74c2b75354fa1ddca8a20c7a
Andrés Morales Reyes <sharker92@hotmail.com>
2025-03-21 08:44:01 -0600

    👌 IMPROVE: Get daily habits NOTION

--- src/notion.ts
+++ src/notion.ts
@@ -9,25 +9,33 @@ const notion = new Client({ auth: process.env.NOTION_KEY });
 
 // const listUsersResponse = await notion.users.list({});
 // console.log(listUsersResponse);
-const habits = await notion.databases.query({
-  database_id: process.env.NOTION_MAIN_DATABASE_ID,
-  filter: {
-    and: [
-      { property: 'Due', date: { is_not_empty: true } },
-      { property: 'Recur Unit', select: { is_not_empty: true } },
-      { property: 'Recur Interval', number: { is_not_empty: true } },
-      { property: 'Occurrences History', relation: { is_not_empty: true } },
-    ],
-  },
-});
-console.log(habits);
-console.log(habits.results.length);
-console.log('notion end');
+async function getHabits(dateArray: Date[]): Promise<void> {
+  for (const date of dateArray) {
+    console.log(date.toISOString());
+    const habits = await notion.databases.query({
+      database_id: process.env.NOTION_MAIN_DATABASE_ID,
+      filter: {
+        and: [
+          {
+            property: 'Completed',
+            date: { equals: date.toISOString().split('T')[0] }, // TODO: ahora sigue hacer el check en el reporte. Jalar la página del día y hacer checks.
+          },
+          { property: 'Recur Unit', select: { is_empty: true } },
+          { property: 'Recur Interval', number: { is_empty: true } },
+          { property: 'Recurrent Parent', relation: { is_not_empty: true } },
+          // { property: 'Task', title: { equals: 'Fuerza' } },
+        ],
+      },
+    });
+    console.log(habits);
+    // console.log(habits.results[0].properties['Occurrences History']);
+    // console.log(habits.results[0].properties['Occurrences History'].relation);
+    console.log(habits.results.length);
+    console.log('notion end');
+  }
+}
+const dateArray = [new Date()];
+getHabits(dateArray);
 
-// Testing in 1 and 2 tables
-// const habits = await notion.databases.query({
-//   database_id: '19bf80c2a4fc80b28517f6e8daa29386',
-// });
-// console.log(habits);
-// console.log(habits.results.length);
+//TODO: la fecha que mandas define desde donde empiezas a jalar datoss
 
